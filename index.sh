6d4a6efccfa30e005e7587ebcdf3366065db9d78
gallen <graham.develops@gmail.com>
2021-01-31 16:17:53 -0800

    Working searching multiple counties

--- index.sh
+++ index.sh
@@ -1,4 +1,11 @@
+counties='Hinds|Madison|Neshoba|Rankin|Attala'
+echo "\n"
+echo Searching for vaccines in the following counties: $counties 
+echo "\n"
 while true
 do
-curl -v --silent https://covidvaccine.umc.edu/ 2>&1 | grep -E 'Hinds|Madison|Neshoba|Rankin|Attala'
+	curl -v --silent https://covidvaccine.umc.edu/ 2>&1 | grep -E $counties;
+	sleep 2;
+	echo "\n"
+	echo "Refreshing";
 done
